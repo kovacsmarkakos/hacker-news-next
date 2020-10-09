159b3ab59279983a9955725452a27819ac437415
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-10-09 16:05:52 +0200

    initial commit: added axios, tested api call

---
+++ services/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
+export const newStoriesUrl = `${baseUrl}newstories.json`;
+export const topStoriesUrl = `${baseUrl}topstories.json`;
+export const showStoriesUrl = `${baseUrl}showstories.json`;
+export const askStoriesUrl = `${baseUrl}askstories.json`;
+export const storyUrl = `${baseUrl}item/`;
+
+export const getStoryIds = async () => {
+  const result = await axios.get(newStoriesUrl).then(({ data }) => data);
+
+  return result;
+};
