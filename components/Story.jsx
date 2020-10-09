e11cf1e376943df63dc273daea3532f4bad10566
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-10-09 18:52:56 +0200

    built out basic structure for story component

--- components/Story.jsx
+++ components/Story.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getStory, getStoryIds } from '../services/api';
+import { getStory } from '../services/api';
 
 export const Story = ({ storyId }) => {
   const [story, setStory] = useState({});
@@ -8,5 +8,13 @@ export const Story = ({ storyId }) => {
     getStory(storyId).then((data) => data && data.url && setStory(data));
   }, []);
 
-  return <p>{JSON.stringify(story)}</p>;
+  return story && story.url ? (
+    <>
+      <a href={story.url} target="_blank" rel="noopener noreferrer">
+        <p>{story.title}</p>
+      </a>
+      By: <p>{story.by}</p>
+      Posted: <p>{story.time}</p>
+    </>
+  ) : null;
 };
--- containers/StoriesContainer.jsx
+++ containers/StoriesContainer.jsx
@@ -1,14 +1,13 @@
 import { useEffect, useState } from 'react';
 import { Story } from '../components/Story';
-import { getStory, getStoryIds } from '../services/api';
+import { getStoryIds } from '../services/api';
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
     getStoryIds().then((data) => setStoryIds(data));
-    getStory(126809).then((data) => console.log(data));
   }, []);
 
-  return storyIds.map((storyId) => <Story storyId={storyId} />);
+  return storyIds.map((storyId) => <Story key={storyId} storyId={storyId} />);
 };
