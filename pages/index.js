53080286b114558def37ca7f6da2a793c29c5a6c
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-10-10 10:25:52 +0200

    code refactoring

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { StoriesContainer } from '../containers/StoriesContainer';
+import { StoriesContainer } from './StoriesContainer';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
--- services/api.js
+++ services/api.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { selectFields } from '../utilities/selectFields';
 
 export const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
-export const newStoriesUrl = `${baseUrl}newstories.json`;
-export const topStoriesUrl = `${baseUrl}topstories.json`;
-export const showStoriesUrl = `${baseUrl}showstories.json`;
-export const askStoriesUrl = `${baseUrl}askstories.json`;
 export const storyUrl = `${baseUrl}item/`;
+export const newStoriesUrl = `${baseUrl}newstories.json`;
+/* export const topStoriesUrl = `${baseUrl}topstories.json`;
+export const showStoriesUrl = `${baseUrl}showstories.json`;
+export const askStoriesUrl = `${baseUrl}askstories.json`; */
 
 export const getStoryIds = async () => {
   const result = await axios.get(newStoriesUrl).then(({ data }) => data);
