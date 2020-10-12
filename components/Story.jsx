8a1237bd302b5d61d5163958d0f7a87cc1c1ad43
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-10-12 14:04:48 +0200

    added timeago function that formats the timestamp

--- components/Story.jsx
+++ components/Story.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getStory } from '../services/api';
 import styles from './Story.module.scss';
+import mapTime from '../mappers/mapTime';
 
 export const Story = ({ storyId }) => {
   const [story, setStory] = useState({});
@@ -16,7 +17,7 @@ export const Story = ({ storyId }) => {
       </a>
       <div className={styles.storyMeta}>
         <span>By: {story.by}</span>
-        <span>Posted: {story.time}</span>
+        <span>Posted: {mapTime(story.time)}</span>
       </div>
     </section>
   ) : null;
