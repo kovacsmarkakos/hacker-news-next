ccffc78fb15371ac2a0ccd98198e87dbead72228
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-10-12 13:41:13 +0200

    styled stories container

--- pages/newstories.jsx
+++ pages/newstories.jsx
@@ -2,17 +2,20 @@ import axios from 'axios';
 import { Story } from '../components/Story';
 import { newStoriesUrl } from '../services/api.js';
 import Link from 'next/link';
+import styles from '../styles/newstories.module.scss';
 
 export default function NewStories({ result }) {
   return (
-    <>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
+    <main className={styles.storiesContainerWrapper}>
+      <nav className={styles.nav}>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </nav>
       {result.map((id) => (
         <Story key={id} storyId={id} />
       ))}
-    </>
+    </main>
   );
 }
 
