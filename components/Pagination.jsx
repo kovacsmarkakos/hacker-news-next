2f0a2dc0253e15abb54851afc6de7b9a5a86df2f
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-10-13 15:23:48 +0200

    added pagination component logic

---
+++ components/Pagination.jsx
@@ -0,0 +1,29 @@
+import styles from './Pagination.module.scss';
+
+const Pagination = ({ storiesPerPage, totalStories, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalStories / storiesPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div className={(styles.container, styles.large)}>
+      <div className={styles.pagination}>
+        <ul className={styles.pageList}>
+          {pageNumbers.map((number) => (
+            <li key={number} className={styles.pageItem}>
+              <a
+                onClick={() => paginate(number)}
+                href="#"
+                className={styles.pageLink}
+              ></a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
--- pages/newstories.jsx
+++ pages/newstories.jsx
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Story } from '../components/Story';
 import { newStoriesUrl } from '../services/api.js';
 import Link from 'next/link';
 import styles from '../styles/newstories.module.scss';
+import Pagination from '../components/Pagination';
 
 export default function NewStories({ result }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [storiesPerPage, setStoriesPerPage] = useState(20);
+
+  // Get current posts
+  const indexOfLastStory = currentPage * storiesPerPage;
+  const indexOfFirstStory = indexOfLastStory - storiesPerPage;
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <main className={styles.storiesContainerWrapper}>
       <nav className={styles.nav}>
@@ -12,7 +24,12 @@ export default function NewStories({ result }) {
           <a>Home</a>
         </Link>
       </nav>
-      {result.map((id) => (
+      <Pagination
+        storiesPerPage={storiesPerPage}
+        totalStories={result.length}
+        paginate={paginate}
+      />
+      {result.slice(indexOfFirstStory, indexOfLastStory).map((id) => (
         <Story key={id} storyId={id} />
       ))}
     </main>
