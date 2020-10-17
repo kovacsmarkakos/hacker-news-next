25c9fd114561eb45e2a464e04b0d5a6c21081470
Mark Kovacs <64911445+kovacsmarkakos@users.noreply.github.com>
2020-10-17 16:02:58 +0200

    removed unused pkgs added favicons

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,32 @@ class MyDocument extends Document {
             content="Hacker News Next is a news reader app, built with Next.js. Fetches data from the Hacker News API, with server-side rendering."
           />
           <link rel="icon" href="/favicon.ico" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/public/icons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/public/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/public/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/public/icons/site.webmanifest" />
+          <link rel="shortcut icon" href="/public/icons/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#212121" />
+          <meta
+            name="msapplication-config"
+            content="/public/icons/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,6 @@ export default function Home({ result }) {
   const [currentPage, setCurrentPage] = useState(1);
   const [storiesPerPage, setStoriesPerPage] = useState(20);
 
-  // Get current posts
   const indexOfLastStory = currentPage * storiesPerPage;
   const indexOfFirstStory = indexOfLastStory - storiesPerPage;
 
